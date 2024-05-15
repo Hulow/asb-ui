e1b9104f5f11ff65da288b3d7b249f55bfbba353
Hulow <viq.hlw@gmail.com>
2024-05-15 11:24:55 +0200

    feat: setup picture component

---
+++ src/components/Picture/Picture.tsx
@@ -0,0 +1,25 @@
+import { CldImage } from 'next-cloudinary';
+
+export function Picture({
+  width,
+  height,
+  src,
+}: {
+  width: number;
+  height: number;
+  src: string;
+}) {
+  return (
+    <CldImage
+      width={width}
+      height={height}
+      src={src}
+      crop={{
+        type: 'auto',
+        source: true,
+      }}
+      alt={''}
+      radius={15}
+    />
+  );
+}
