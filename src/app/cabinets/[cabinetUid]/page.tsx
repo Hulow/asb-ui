4a5fb35d5bb2995cd2e88e8d3ff2e798da75ac7d
Hulow <viq.hlw@gmail.com>
2024-05-15 18:13:39 +0200

    feat: implement speaker overview

--- src/app/cabinets/[cabinetUid]/page.tsx
+++ src/app/cabinets/[cabinetUid]/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import './page.css';
 import { useEffect, useState } from 'react';
 import { config } from '../../../config/config';
 import { Measurement } from '../../../types/measurements';
+import texts from '../../../data/texts.json';
+import { Picture } from '../../../components/Picture/Picture';
 
 interface Params {
   params: {
@@ -32,5 +35,121 @@ export default function MeasurementPage({ params }: Params) {
     return <div></div>;
   }
 
-  return <div></div>;
+  return (
+    <main className='measurement flex-column-center'>
+      <h1>{texts.measurements}</h1>
+      <div className='flex-center'>
+        <Picture height={400} width={500} src='cld-sample-5' />
+        <div className='speaker-overview flex-column-center component'>
+          <h1>Cabinet</h1>
+          <div className='speaker-items'>
+            <div className='speaker-item'>
+              <div className='speaker-sub-item'>
+                <p>Name</p>
+              </div>
+              <div className='speaker-sub-item'>
+                <p>{measurements?.cabinet.productName}</p>
+              </div>
+            </div>
+            <div className='speaker-item'>
+              <div className='speaker-sub-item'>
+                <p>Enclosure</p>
+              </div>
+              <div className='speaker-sub-item'>
+                <p>{measurements?.cabinet.enclosureType}</p>
+              </div>
+            </div>
+            <div className='speaker-item'>
+              <div className='speaker-sub-item'>
+                <p>Dimension</p>
+              </div>
+              <div className='speaker-sub-item'>
+                <p>{measurements?.cabinet.dimension}</p>
+              </div>
+            </div>
+            <div className='speaker-item'>
+              <div className='speaker-sub-item'>
+                <p>Year</p>
+              </div>
+              <div className='speaker-sub-item'>
+                <p>{measurements?.cabinet.manufacturingYear}</p>
+              </div>
+            </div>
+            <div className='speaker-item'>
+              <div className='speaker-sub-item'>
+                <p>Weight</p>
+              </div>
+              <div className='speaker-sub-item'>
+                <p>{measurements?.cabinet.weight} Kg</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {measurements?.drivers.map((driver) => {
+          return (
+            <div className='speaker-overview flex-column-center component'>
+              <h1>Driver</h1>
+              <div className='speaker-items'>
+                <div className='speaker-item'>
+                  <p>Manufacturer</p>
+                  <p>{driver.brandName}</p>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>Product</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.productName}</p>
+                  </div>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>Type</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.driverType}</p>
+                  </div>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>RMS</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.continuousPowerHandling} W</p>
+                  </div>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>Diameter</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.nominalDiameter} Inch</p>
+                  </div>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>Impedance</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.nominalImpedance} Ohm</p>
+                  </div>
+                </div>
+                <div className='speaker-item'>
+                  <div className='speaker-sub-item'>
+                    <p>Year</p>
+                  </div>
+                  <div className='speaker-sub-item'>
+                    <p>{driver.manufacturingYear}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex-center">
+        
+      </div>
+    </main>
+  );
 }
