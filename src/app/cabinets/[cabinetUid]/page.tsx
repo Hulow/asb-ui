eb41477c5aa811b2abb05c533f93caa6c3cd6052
Hulow <viq.hlw@gmail.com>
2024-05-16 12:29:42 +0200

    chore: refactor measurements style

--- src/app/cabinets/[cabinetUid]/page.tsx
+++ src/app/cabinets/[cabinetUid]/page.tsx
@@ -80,42 +80,42 @@ export default function MeasurementPage({ params }: Params) {
           <h1>Cabinet</h1>
           <div className='speaker-items'>
             <div className='flex-row'>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>Name</p>
               </div>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>{measurements?.cabinet.productName}</p>
               </div>
             </div>
             <div className='flex-row'>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>Enclosure</p>
               </div>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>{measurements?.cabinet.enclosureType}</p>
               </div>
             </div>
             <div className='flex-row'>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>Dimension</p>
               </div>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>{measurements?.cabinet.dimension}</p>
               </div>
             </div>
             <div className='flex-row'>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>Year</p>
               </div>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>{measurements?.cabinet.manufacturingYear}</p>
               </div>
             </div>
             <div className='flex-row'>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>Weight</p>
               </div>
-              <div className='speaker-sub-item'>
+              <div className='item speaker'>
                 <p>{measurements?.cabinet.weight} Kg</p>
               </div>
             </div>
@@ -130,58 +130,58 @@ export default function MeasurementPage({ params }: Params) {
               <h1>Driver</h1>
               <div className='speaker-items'>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Manufacturer</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.brandName}</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Product</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.productName}</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Type</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.driverType}</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>RMS</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.continuousPowerHandling} W</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Diameter</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.nominalDiameter} Inch</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Impedance</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.nominalImpedance} Ohm</p>
                   </div>
                 </div>
                 <div className='flex-row'>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>Year</p>
                   </div>
-                  <div className='speaker-sub-item'>
+                  <div className='item speaker'>
                     <p>{driver.manufacturingYear}</p>
                   </div>
                 </div>
@@ -195,38 +195,38 @@ export default function MeasurementPage({ params }: Params) {
         <Chart props={frequencyChat} />
       </div>
       <div className='settings flex-row'>
-        <div className='settings-item component flex-center'>
+        <div className='item horizontal-padding component flex-center'>
           <h1>Settings</h1>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Audio Interface</p>
           <p>{measurements.frequency.source}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Measured at</p>
           <p>{measurements.frequency.measuredAt}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Measured by</p>
           <p>{measurements.frequency.measuredBy}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Smoothing</p>
           <p>{measurements.frequency.smoothing}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Weightings</p>
           <p>{measurements.frequency.frequencyWeightings}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Sweep length</p>
           <p>{measurements.frequency.sweepLength}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Target</p>
           <p>{measurements.frequency.targetLevel}</p>
         </div>
-        <div className='settings-item component note'>
+        <div className='item horizontal-padding component note'>
           <p>Note</p>
           <p>{measurements.frequency.note}</p>
         </div>
@@ -236,71 +236,71 @@ export default function MeasurementPage({ params }: Params) {
         <Chart props={impedanceChart} />
       </div>
       <div className='settings flex-row'>
-        <div className='settings-item component flex-center'>
+        <div className='item horizontal-padding component flex-center'>
           <h1>TS Parameters</h1>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Measured by</p>
           <p>{measurements.impedance.source}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>F(s)</p>
-          <p>{measurements.impedance.resonanceFrequency} Hertz</p>
+          <p>{measurements.impedance.resonanceFrequency} Hz</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>AC Res.</p>
           <p>{measurements.impedance.acResistance} Ohms</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>DC Res.</p>
           <p>{measurements.impedance.dcResistance}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Q(ts)</p>
           <p>{measurements.impedance.totalDamping}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Q(es)</p>
           <p>{measurements.impedance.electricalDamping}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Q(ms)</p>
           <p>{measurements.impedance.mechanicalDamping}</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>V(as)</p>
           <p>{measurements.impedance.equivalenceCompliance} L</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>C(ms)</p>
           <p>{measurements.impedance.suspensionCompliance} mm/N</p>
         </div>
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Cone Mass</p>
           <p>{measurements.impedance.coneMass} g</p>
         </div>
 
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Efficiency</p>
           <p>{measurements.impedance.efficiency} %</p>
         </div>
 
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>BL</p>
           <p>{measurements.impedance.forceFactor}</p>
         </div>
 
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>Piston</p>
           <p>⌀ {measurements.impedance.pistonDiameter} mm</p>
         </div>
 
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>SPL</p>
           <p>{measurements.impedance.sensitivity} 1W/1M</p>
         </div>
 
-        <div className='settings-item component'>
+        <div className='item horizontal-padding component'>
           <p>L(e)</p>
           <p>{measurements.impedance.voiceCoilInductance} mH</p>
         </div>
