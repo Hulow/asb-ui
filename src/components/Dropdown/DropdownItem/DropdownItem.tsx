3207484c6b7faeab2033bfd3f1e4748b04ee7ce3
Hulow <viq.hlw@gmail.com>
2024-05-18 01:11:55 +0200

    chore: update dropdown item component style

--- src/components/Dropdown/DropdownItem/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem/DropdownItem.tsx
@@ -9,19 +9,19 @@ export const DropdownItem = ({
 }) => {
   return (
     <div className='dropdown-item component' onClick={selectProp}>
-      <h2>{owner}</h2>
+      <h1>{owner}</h1>
     </div>
   );
 
   function selectProp(event: any): void {
-    const h2Tags = document.querySelectorAll('.dropdown-item h2');
-    const targetOwner = getTargetOwner(h2Tags, event.target.innerHTML);
+    const h1Tags = document.querySelectorAll('.dropdown-item h1');
+    const targetOwner = getTargetOwner(h1Tags, event.target.innerHTML);
     selectedOwner(targetOwner);
   }
 
   function getTargetOwner(h2Tags: NodeListOf<Element>, event: string): string {
-    for (const h2 of h2Tags) {
-      const h2Content = h2.textContent as string;
+    for (const h1 of h2Tags) {
+      const h2Content = h1.textContent as string;
       if (matchEvent(h2Content, event)) return h2Content;
     }
     return event;
