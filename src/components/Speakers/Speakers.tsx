da3a4d7ba75c6bb2cde0c85c035be7a66e9f37f0
Hulow <viq.hlw@gmail.com>
2024-05-18 01:08:59 +0200

    chore: update speaker component style

--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -14,7 +14,7 @@ export function Speakers({
 }) {
   return (
     <div className='speakers-props flex-column-center component'>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <div className='speaker-props'>
         {props.map((prop, index) => {
           return <Speaker prop={prop} key={index} />;
@@ -27,10 +27,10 @@ export function Speakers({
 export function Speaker({ prop }: { prop: Property }) {
   return (
     <div className='flex-row'>
-      <div className='speaker item'>
+      <div className='speaker'>
         <p>{prop.name}</p>
       </div>
-      <div className='speaker item'>
+      <div className='speaker'>
         <p>{prop.value}</p>
       </div>
     </div>
