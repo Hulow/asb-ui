54951664e578ac5d0c961fc4fa74bfc88970e27b
Hulow <viq.hlw@gmail.com>
2024-05-21 12:39:08 +0200

    fix: error eslint in footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,18 +6,14 @@ export function Footer() {
   return (
     <div className='footer layout'>
       <div className='asb-ui'>
-        <CustomLink
-          href={texts.asbUiUrl}
-          children={<h4>{texts.asbUi}</h4>}
-          target={true}
-        />
+        <CustomLink href={texts.asbUiUrl} target={true}>
+          <h4>{texts.asbUi}</h4>
+        </CustomLink>
       </div>
       <div className='asb-app'>
-        <CustomLink
-          href={texts.asbAppUrl}
-          children={<h4>{texts.asbApp}</h4>}
-          target={true}
-        />
+        <CustomLink href={texts.asbAppUrl} target={true}>
+          <h4>{texts.asbApp}</h4>
+        </CustomLink>
       </div>
       <div className='email'>
         <h4>{texts.emailAddress}</h4>
