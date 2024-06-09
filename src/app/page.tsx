b891875197f25967c60940ad0f7b38b6bc7a04e8
Hulow <viq.hlw@gmail.com>
2024-06-09 18:19:38 +0200

    fix: cabinet endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    fetch(`${endpoint}`, {
+    fetch(`${config.asbBaseUrl}${endpoint}`, {
       headers: { Authorization: config.asbKeyUrl },
     })
       .then((res) => res.json())
