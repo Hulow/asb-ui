775e8fab3b0b0d2d2aa259f83260df2e04969b75
Hulow <viq.hlw@gmail.com>
2024-06-09 18:19:54 +0200

    chore: update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  // Your Next.js config options...
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-
-  // Define public runtime variables
   env: {
     NEXT_PUBLIC_ASB_BASE_URL: process.env.NEXT_PUBLIC_ASB_BASE_URL,
     NEXT_PUBLIC_ASB_KEY_URL: process.env.NEXT_PUBLIC_ASB_KEY_URL,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    NEXT_PUBLIC_CLOUDINARY_API_SECRET:
+      process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
   },
 };
+
+module.exports = nextConfig;
