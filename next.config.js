81fa8fe29ed0a2241f760f68136aed031b266ff7
Hulow <viq.hlw@gmail.com>
2024-07-07 20:18:49 +0200

    feat: add env to cabinet picture name

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-    NODE_ENV: process.env.NODE_ENV,
+    ENV: process.env.NODE_ENV,
   },
   images: {
     remotePatterns: [
