4d8fe02d0ffffbfc49a389a426b3675259637762
Hulow <viq.hlw@gmail.com>
2024-07-08 12:29:59 +0200

    chore: remove header metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,6 @@ export default function Layout({
 }>) {
   return (
     <html lang='en'>
-      <head>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-        />
-      </head>
       <body>
         {children}
         <Footer />
