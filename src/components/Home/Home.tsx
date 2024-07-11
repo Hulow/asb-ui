765580f02ec2f9f86498753ae47b778923d0b8c8
Hulow <viq.hlw@gmail.com>
2024-07-11 20:22:26 +0200

    chore: refactor home page to server side rendering

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,38 +1,37 @@
-'use client'
+'use client';
 
 import texts from '../../data/texts.json';
-import { useState } from "react";
-import { CabinetOverview } from "../../types/cabinet-overview";
-import { NavBar, OnEvent } from "../NavBar/NavBar";
-import { ProjectOverview } from "../ProjectOverview/ProjectOverview";
-import { Station } from "../Station/Station";
-import { Cabinets } from "../Cabinets/Cabinets";
+import { useState } from 'react';
+import { CabinetOverview } from '../../types/cabinet-overview';
+import { NavBar, OnEvent } from '../NavBar/NavBar';
+import { ProjectOverview } from '../ProjectOverview/ProjectOverview';
+import { Station } from '../Station/Station';
+import { Cabinets } from '../Cabinets/Cabinets';
 import { Logo } from '../Image/Logo';
 
-export const HomePage = ({cabinets}: {cabinets: CabinetOverview[]}) => {
-    const [event, setEvent] = useState<undefined | OnEvent>(undefined);
-  
-    const handleEvent = (event: any): void => {
-      setEvent(event);
+export const HomePage = ({ cabinets }: { cabinets: CabinetOverview[] }) => {
+  const [event, setEvent] = useState<undefined | OnEvent>(undefined);
+
+  const handleEvent = (event: any): void => {
+    setEvent(event);
+  };
+
+  const Content = ({ event }: { event: OnEvent }) => {
+    switch (event?.name) {
+      case texts.projectOverview:
+        return <ProjectOverview />;
+      case texts.about:
+        return <Station />;
+      case texts.measurements:
+        return <Cabinets cabinets={cabinets} />;
     }
-  
-    const Content = ({ event }: { event: OnEvent }) => {
-      switch (event?.name) {
-        case texts.projectOverview:
-          return <ProjectOverview />;
-        case texts.about:
-          return <Station />;
-        case texts.measurements:
-          return <Cabinets cabinets={cabinets} />;
-      }
-    }
-  
-    return (
-      <main className='flex-col-center'>
-        <Logo route='/ASB.svg' alt='Anechoic Station Berlin' />
-        <NavBar onEvent={handleEvent} />
-        {event?.state ? <Content event={event} /> : null}
-      </main>
-    );
-  }
-  
+  };
+
+  return (
+    <main className='flex-col-center'>
+      <Logo route='/ASB.svg' alt='Anechoic Station Berlin' />
+      <NavBar onEvent={handleEvent} />
+      {event?.state ? <Content event={event} /> : null}
+    </main>
+  );
+};
