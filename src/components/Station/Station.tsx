93629756eaa8bc6a797136b348cb9c6856f10867
Hulow <viq.hlw@gmail.com>
2024-07-23 17:25:11 +0200

    feat: setup station component

--- src/components/Station/Station.tsx
+++ src/components/Station/Station.tsx
@@ -1,5 +1,37 @@
 import '../../styles/components/station.scss';
 
 export function Station() {
-  return <div>STATION COMPONENT</div>;
+  return (
+    <div className='station flex-col'>
+      <div>
+        <h1>Overview</h1>
+        <p>Who I am </p>
+        <p>Localisation</p>
+        <p>Goal</p>
+      </div>
+      <div>
+        <h1>Station</h1>
+        <p>Area and volume</p>
+      </div>
+      <div>
+        <h1>Materiaux</h1>
+        <p>room itselve and cones</p>
+      </div>
+      <div>
+        <h1>Software</h1>
+        <p>Hexa</p>
+        <p>Classic web app SSR</p>
+      </div>
+      <div>
+        <h1>Measurements</h1>
+        <p>Technics</p>
+        <p>Software used</p>
+        <p>Hardware used</p>
+      </div>
+      <div>
+        <h1>Feedback from the room</h1>
+        <p>Low frequency because of the size</p>
+      </div>
+    </div>
+  );
 }
