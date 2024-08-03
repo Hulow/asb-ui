2e388bdce034050c1943f1e3c0d75d4afe1f03cc
Hulow <viq.hlw@gmail.com>
2024-08-03 15:02:41 +0200

    chore: update project description test

--- src/components/Station/Station.tsx
+++ src/components/Station/Station.tsx
@@ -6,21 +6,20 @@ export function Station() {
       <div>
         <h1>About the project</h1>
         <p>
-          Hello my name is Victor and here is my side project related to
-          acoustics. With a keen interest in loudspeaker design, I had the idea
-          to build a semi anechoic chamber in a Soviet bunker located in
-          southern Berlin.
+          Hello, my name is Victor, and this is my project related to acoustics.
+          With a keen interest in loudspeaker design, I had the idea to build a
+          semi anechoic chamber in a Soviet bunker located in southern Berlin.
         </p>
         <p>
           This room is focused on delivering accurate electro-acoustic
           measurements on loudspeakers by throwing an impulse signal, recording
-          it via a microphone and processing it via software.
+          it via a microphone, and processing it via software.
         </p>
         <p>
           Believing that collaboration and transparency accelerate learning,
           this project is committed to three key principles:
           <ul>
-            <li>The project is non profit-oriented.</li>
+            <li>The project is non-profit-oriented.</li>
             <li>The room is open to everyone.</li>
             <li>
               All data recorded from this room is publicly accessible via this
@@ -30,16 +29,17 @@ export function Station() {
         </p>
         <p>
           Currently, this web application provides frequency and impedance
-          response plots. As mentioned before, this room is a side project and I
-          will need additional time to deliver more analysis through this
-          application…
+          response plots. As mentioned, this room is a side project and so I
+          will need additional time to deliver more loudspeaker analysis and
+          make the application more responsive.
         </p>
       </div>
       <div>
         <h1>About the room</h1>
         <p>
           The room covers a floor area of 20 m² and a volume of 38 m³. The size
-          of the room could not be bigger due to a budget constraint.
+          of the room could not be bigger due to a budget constraint. Due to a
+          budget constraint, the room could not be bigger.
         </p>
         <p>
           The walls, door, ceiling, and floor are insulated with a 200 mm thick
@@ -47,20 +47,20 @@ export function Station() {
         </p>
         <p>
           Additionally, a 16 mm perforated OSB layer has been applied to the
-          walls and ceiling acting as Helmholtz resonators.
+          walls and ceiling, acting as Helmholtz resonators.
         </p>
         <p>
           Approximately 200 absorbers are installed in the room. Each absorber
           consists of a fine layer of fiberglass (190 g/m²) coupled with
           polyester glass. The structure of these absorbers includes both an
-          inner and outer layer of rockwool improving sound absorption.
+          inner and outer layer of rockwool, improving sound absorption.
         </p>
       </div>
       <div>
         <h1>About the measurements</h1>
         <p>
           Each acoustic measurement uses the ground plane measurement technique
-          to minimise floor reflections.
+          in order to minimise floor reflections.
         </p>
         <p>
           Tests are conducted on axis with a microphone positioned between 1 to
@@ -81,20 +81,20 @@ export function Station() {
       </div>
       <div>
         <h1>Applications overview</h1>
-        <p>This part is more geek oriented.</p>
+        <p>This part is more geek-oriented.</p>
         <p>
           ASB APP and ASB UI are the applications designed for processing and
           rendering measurements on the web. Both repositories are available on
           github.
         </p>
         <p>
-          Acts as a backend, processing all measurements and storing them in a
-          PostgreSQL database. It uses an IoC container provided by InversifyJS
-          and follows the Hexagonal Architecture pattern introduced by Alistair
-          Cockburn.
+          ASB APP acts as a backend, processing all measurements and storing
+          them in a PostgreSQL database. It uses an IoC container provided by
+          InversifyJS and follows the Hexagonal Architecture pattern introduced
+          by Alistair Cockburn.
         </p>
         <p>
-          ASB UI: Serves as a frontend web application for rendering data, built
+          ASB UI serves as a frontend web application for rendering data, built
           with Next.js for server-side rendering. Lots of things have to be
           improved there since I have never had any proper experience with
           frontend development…
