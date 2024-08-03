a3740d0feca61786b9159c144fb6f9df41ce6f81
Hulow <viq.hlw@gmail.com>
2024-08-03 11:48:15 +0200

    feat: add new room picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,43 @@
+import path from 'path';
 import { HomePage } from '../components/Home/Home';
 import { config } from '../config/config';
 import { asbHandler } from '../handlers/asb';
-import { cloudinaryHandler } from '../handlers/cloudinary';
+import { PictureMetadata } from '../handlers/cloudinary';
+import sizeOf from 'image-size';
 
 export default async function Home() {
   const asbEndpoint = `${config.asbBaseUrl}${config.endpoints.cabinets}`;
-  const cloudinaryEndpoint = `${config.cloudinary.apiUrl}?public_ids=chamber%2Froom`;
-  const [cabinets, pictureMetadata] = await Promise.all([
-    await asbHandler(asbEndpoint),
-    await cloudinaryHandler(cloudinaryEndpoint),
-  ]);
+  const cabinets = await asbHandler(asbEndpoint);
+  const pictureMetadata = getPictureMetadata();
 
   if (!cabinets.length || !pictureMetadata) {
     return <div>Web Application in maintenance</div>;
   }
 
   return <HomePage cabinets={cabinets} pictureMetadata={pictureMetadata} />;
+}
+
+function getPictureMetadata(): PictureMetadata {
+  const imagesPath = path.resolve('./public/anechoic_chamber.jpg');
+  const dimension = sizeOf(imagesPath);
+  return mapPictureDimension(dimension, '/anechoic_chamber.jpg');
+}
+
+function mapPictureDimension(
+  dimension: PictureDimension,
+  url: string
+): PictureMetadata {
+  return {
+    url,
+    height: dimension.height!,
+    width: dimension.width!,
+    ratio: 'string',
+  };
+}
+
+export interface PictureDimension {
+  width?: number;
+  height?: number;
+  type?: string;
+  orientation?: number;
 }
